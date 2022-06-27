4b13ea625fd129726bd85b77c2d6037212ba2f75
JoelmaRegina <joelma_silvaa@hotmail.com>
2022-06-26 23:07:24 -0300

    Created the API

---
+++ API/ExoAPI/ExoAPI/Contexts/ChapterContext.cs
@@ -0,0 +1,28 @@
+﻿using ExoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExoAPI.Contexts
+{
+    public class ChapterContext : DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext>options) : base(options)
+        {
+        }
+        // vamos utilizar esse método para configurar o banco dedados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe paraespecificação
+                optionsBuilder.UseSqlServer("Data Source = LAPTOP-0KP4FMIJ\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro> Livros { get; set; }
+
+    }
+}
---
+++ API/ExoAPI/ExoAPI/Repositories/LivroRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExoAPI.Contexts;
+using ExoAPI.Models;
+
+namespace ExoAPI.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly ChapterContext _context;
+        public LivroRepository(ChapterContext context)
+        {
+            _context = context;
+        }
+
+        public List<Livro> Listar() 
+        {
+            return _context.Livros.ToList();
+        }
+    }
+}
