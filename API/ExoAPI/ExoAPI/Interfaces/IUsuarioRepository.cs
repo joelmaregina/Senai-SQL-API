44e3cb358180cfe9cc4e3914269bf09536a2f9fc
JoelmaRegina <joelma_silvaa@hotmail.com>
2022-06-27 23:23:09 -0300

    Added User CRUD and Cors

---
+++ API/ExoAPI/ExoAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,19 @@
+﻿using ExoAPI.Models;
+
+namespace ExoAPI.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> Listar();
+
+        Usuario BuscarPorId(int id);
+
+        void Cadastrar(Usuario usuario);
+
+        void Atualizar(int id, Usuario usuario);
+
+        void Deletar(int id);
+
+        Usuario Login(string email, string senha);
+    }
+}
--- API/ExoAPI/ExoAPI/Program.cs
+++ API/ExoAPI/ExoAPI/Program.cs
@@ -1,4 +1,5 @@
 using ExoAPI.Contexts;
+using ExoAPI.Interfaces;
 using ExoAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,8 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors( options =>
+{
+    options.AddPolicy("CorsPolicy", builder => 
+    {
+        builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddScoped<ChapterContext, ChapterContext>();
-builder.Services.AddTransient<LivroRepository, LivroRepository>();
+builder.Services.AddTransient<LivroRepository, LivroRepository>(); // S/ Interface
+builder.Services.AddTransient<IUsuarioRepository, UsuarioRepository>(); //C/ interface
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
