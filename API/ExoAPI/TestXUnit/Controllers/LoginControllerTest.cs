1c90a5c8b070987023d49187500453d2a1ce64e5
JoelmaRegina <joelma_silvaa@hotmail.com>
2022-06-29 18:17:55 -0300

    Integration Test of the API

---
+++ API/ExoAPI/TestXUnit/Controllers/LoginControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using ExoAPI.Controllers;
+using ExoAPI.Interfaces;
+using ExoAPI.Models;
+using ExoAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestXUnit.Controllers
+{
+    public class LoginControllerTest
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            //Arrange
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+
+            //Act
+
+            var resultado = controller.Login(dadosLogin);
+
+            //Assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+
+        }
+
+        [Fact]
+        public void LoginController_Retornar_Token()
+        {
+            //Arrange
+            Usuario usuarioRetorno = new Usuario();
+            usuarioRetorno.Email = "email@email.com";
+            usuarioRetorno.Senha = "123";
+            usuarioRetorno.Tipo = "0";
+
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns(usuarioRetorno);
+
+            string issuerValidacao = "chapter.webapi";
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+
+            dadosLogin.Email = "qualquercoisa";
+            dadosLogin.Senha = "123";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //Act
+            OkObjectResult resultado = (OkObjectResult)controller.Login(dadosLogin);
+
+            string token = resultado.Value.ToString().Split(' ')[3];
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+
+            var tokenJwt = jwtHandler.ReadJwtToken(token);
+
+            //Assert
+            Assert.Equal(issuerValidacao, tokenJwt.Issuer);
+        }
+    }
+}
